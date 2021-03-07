0eea91309b069fa0c91b6a64ddba25b0e0ecbd2c
patrick-rush <patrick.a.rush@gmail.com>
2021-03-06 21:46:03 -0500

    commit tree 61bb61b1d2e7efc6cac4ef394f3aa793b8506faf

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,20 @@
-// write your CatList component here
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class CatList extends Component {
+
+    // renderCatPics = () => this.props.catPics.map(catPic => {debugger})
+    renderCatPics = () => this.props.catPics.map(catPic => <img src={catPic.url} />)
+
+    render() {
+        return (
+            <div>
+                {this.renderCatPics()}
+            </div> 
+        )
+    }
+    
+    
+}
+
+export default CatList;
